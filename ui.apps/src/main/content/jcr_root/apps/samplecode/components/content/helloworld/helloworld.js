5f0c901d5bb0fb4ac1cf8dda33f21649dfad4489
nikhil <lnikhilreddy@gmail.com>
2016-02-29 10:11:36 -0500

    Nikhil: Beautify the html for more understanding

---
+++ ui.apps/src/main/content/jcr_root/apps/samplecode/components/content/helloworld/helloworld.js
@@ -0,0 +1,18 @@
+"use strict";
+use(function () {
+    var info = {};    
+    // Does not work
+    info.helloWorld 		= sling.getService(Packages.org.nikhil.SampleCode.core.models.HelloWorldModel);
+    
+    // Does not work
+    info.hw					= org.nikhil.SampleCode.core.models.HelloWorldModel;
+    
+    // Works
+    info.ss					= sling.getService(Packages.org.nikhil.SampleCode.core.service.SampleService);
+    
+    // Works
+    info.ssimpl				= org.nikhil.SampleCode.core.service.SampleServiceImpl;
+    //info.str				= org.nikhil.SampleCode.core.service.SampleServiceImpl.getHello();
+    
+    return info;
+});
